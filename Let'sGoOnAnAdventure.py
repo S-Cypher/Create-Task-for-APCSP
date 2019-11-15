a92e91282ed16c060efe6ff14b05aca211d9f1fb
Keira Stewart <53539678+S-Cypher@users.noreply.github.com>
2019-11-15 07:34:16 -0600

    Update Let'sGoOnAnAdventure.py

--- Let'sGoOnAnAdventure.py
+++ Let'sGoOnAnAdventure.py
@@ -17,20 +17,19 @@ def story_start():
     sleep(0.8)
     print("You've been traveling for a long time and you're tired but, the cave intrigues you.")
     sleep(0.8)
-    print("Do you travel in the cave or make a campsite?")
-    choice1.pack(side = tk.LEFT)
-    choice2.pack(side = tk.LEFT)
-
+    print("You decide to explore the cave.")
+    sleep(1.8)
+    cave()
     
 def cave():
     os.system('cls')
-    print("You chose to explore the cave.")
+    print("The cave is dark but you found a torch on the way in.")
     sleep(0.8)
     print("You come across a sword on the ground")
     sleep(0.8)
     print("Do you pick up the sword or leave it?")
-    choice1.configure(text="Pick up",command=sword_true)
-    choice2.configure(text="Leave it",command=monster)
+    choice1.pack(side=tk.LEFT)
+    choice2.pack(side = tk.LEFT)
 
 def sword_true():
     global sword
@@ -126,42 +125,10 @@ def forward_path():
 def right_path():
     os.system('cls')
     print("You turn right")
-#############################    
-def campsite():
-    os.system('cls')
-    print("You chose to set up a campsite with a small tent.")
-    sleep(0.8)
-    print("Since it's pretty late, you fall asleep")
-    sleep(0.8)
-    print(".")
-    sleep(0.8)
-    print(".")
-    sleep(0.8)
-    print(".")
-    sleep(0.8)
-    print("Morning comes and you set out to find a way out of the forest.")
-    sleep(0.8)
-    print("You start walking and you come to a fork in the road.")
-    sleep(0.8)
-    choosing_paths()
 
-def choosing_paths():
-    print("Do you take the left or right path?")
-    choice1.configure(text="Left",command=village)
-    choice2.configure(text="Right",command=small_hut)
-
-def small_hut():
-    os.system('cls')
-    print("You took the right path and came upon a small hut.")
-    
-
-def village():
-    os.system('cls')
-    print("You took the left path and found a small hut")
-    
 
 root = tk.Tk()
-root.title("Let's Go On An Adventure!")
+root.title("Cave Adventure!")
 frame = tk.Frame(root)
 frame.pack()
 
@@ -169,13 +136,13 @@ bottomframe = tk.Frame(root)
 bottomframe.pack()
 ####
 stop = tk.Button(bottomframe,text="Quit",fg="red",command=quit)
-stop.pack(side=tk.BOTTOM)
+
 
 start = tk.Button(frame,text="Start", activebackground = "aliceblue",command=story_start)
 start.pack(side=tk.LEFT)
 
-choice1 = tk.Button(frame,text="Cave",command=cave)
-choice2 = tk.Button(frame,text="Campsite",command=campsite)
+choice1 = tk.Button(frame,text="Pick up",command=sword_true)
+choice2 = tk.Button(frame,text="Leave it",command=monster)
 choice3 = tk.Button(frame, text= "Right", command = right_path)
 
 root.mainloop()
